35d0160bc22590de762caa3641b4359fe188768c
joel <joel@maraudersoftware.com>
2016-04-11 15:33:38 -0700

    Basic menu for server

--- BigQChatServer/BigQChatServer.cs
+++ BigQChatServer/BigQChatServer.cs
@@ -44,8 +44,55 @@ namespace BigQChat
             Console.WriteLine("");
             Console.WriteLine("BigQ Chat Server");
             Console.WriteLine("Listening on TCP/" + port);
-            Console.WriteLine("Press ENTER to exit");
             Console.WriteLine("");
+
+            bool runForever = true;
+            List<BigQClient> clients;
+
+            while (runForever)
+            {
+                Console.Write("Command [? for help] > ");
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input)) continue;
+
+                switch (input.ToLower().Trim())
+                {
+                    case "?":
+                        Console.WriteLine("-------------------------------------------------------------------------------");
+                        Console.WriteLine("Menu");
+                        Console.WriteLine("  q      quit");
+                        Console.WriteLine("  cls    clear screen");
+                        Console.WriteLine("  list   list connections");
+                        Console.WriteLine("");
+                        break;
+
+                    case "c":
+                    case "cls":
+                        Console.Clear();
+                        break;
+
+                    case "list":
+                        clients = server.ListClients();
+                        if (clients == null) Console.WriteLine("(null)");
+                        else if (clients.Count < 1) Console.WriteLine("(empty)");
+                        else
+                        {
+                            Console.WriteLine(clients.Count + " clients connected");
+                            foreach (BigQClient curr in clients)
+                            {
+                                Console.WriteLine("  " + curr.IpPort() + "  " + curr.ClientGuid + "  " + curr.Email);
+                            }
+                        }
+                        break;
+
+                    case "q":
+                        runForever = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
 
             Console.ReadLine();
         }
